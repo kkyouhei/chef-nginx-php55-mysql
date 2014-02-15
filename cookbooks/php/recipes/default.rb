a1bb52e9a2b64e59d4973a679c3543d3565a4a9c
kkyouhei <hope637@gmail.com>
2014-02-16 02:52:12 +0900

    サードパーティのクックブックを使わずremiレポジトリを追加

--- cookbooks/php/recipes/default.rb
+++ cookbooks/php/recipes/default.rb
@@ -6,9 +6,21 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+remote_file "/tmp/#{node['remi']['file_name']}" do
+     source "#{node['remi']['remote_uri']}"
+     not_if { ::File.exists?("/tmp/#{node['remi']['file_name']}") }
+end
+
+package node['remi']['file_name'] do
+    action :install
+    provider Chef::Provider::Package::Rpm
+    source "/tmp/#{node['remi']['file_name']}"
+end
+
 %w{php php-devel php-mbstring php-fpm php-mysql php-common php-pear}.each do |pkg|
     package pkg do 
         action :install
+        options "--enablerepo=remi --enablerepo=remi-php55"
     end
 
     package "php" do
